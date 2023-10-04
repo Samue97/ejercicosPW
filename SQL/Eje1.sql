c16e6f59f7b60145127afb9f3aa70418bca7803f
Samue97 <samuelromerocaballero@hotmail.com>
2023-10-04 19:28:17 +0200

    Update Eje1.sql

--- SQL/Eje1.sql
+++ SQL/Eje1.sql
@@ -8,8 +8,18 @@ CREATE TABLE IF NOT EXISTS clientes (
  EMail VARCHAR (255)
 )
 
-2. Insertar un nuevo cliente en la tabla "Clientes" con id=1, nombre="Juan" y
-email="juan@example.com"
+/*2. Insertar un nuevo cliente en la tabla "Clientes" con id=1, nombre="Juan" y
+email="juan@example.com"/*
 
 INSERT INTO public.clientes (nombre, email)
 VALUES ('Juan', 'juan@example.com')
+/*3. Actualizar el email del cliente con id=1 a "juan@gmail.com"/*
+UPDATE public.clientes 
+SET email = 'juan@gmail.com'
+WHERE id = 1;
+/* 4. Eliminar el cliente con id=1 de la tabla "Clientes"/*
+DELETE FROM public.clientes
+WHERE id = 1;
+/*5. Crear una tabla llamada "Pedidos" con las columnas: id (entero, clave primaria),
+cliente_id (entero, clave externa referenciando a la tabla "Clientes"), producto
+(texto) y cantidad (entero)/*
